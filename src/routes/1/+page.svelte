9dcc053272b6b04d7213a1698164dd1babed783c
hyuckkim <monkeyhyuck@naver.com>
2024-08-31 16:03:10 +0900

    Init with Svelte-canvas

---
+++ src/routes/1/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import {Canvas, Layer} from 'svelte-canvas';
+</script>
+
+<Canvas
+	on:touchstart={(e) => {
+		e.preventDefault();
+	}}>
+	<Layer render={({
+		context: ctx,
+		width: w,
+		height: h,
+		time: t
+	}) => {
+		ctx.fillStyle = 'black';
+		ctx.fillRect(w/4, h/4, w/2, h/2);
+	}} />
+</Canvas>
