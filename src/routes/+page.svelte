3c740509824e8210d9ed983d4b6bb7dd631dfc1d
hyuckkim <monkeyhyuck@naver.com>
2024-09-09 19:43:03 +0900

    misc: add text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <script>
 	import Card from './_c/card.svelte';
 </script>
-
+<header>
+	<h1>매커니즘 연구소</h1>
+	<small>hyuckkim</small>
+</header>
 <Card
 	title={"1"}
 	description="손가락 세 개로 화살을 날립니다...
  1분의 제한시간이 있습니다."
 	link="/1"
 	/>
+
+<style>
+	header {
+		padding: 2px 12px;
+	}
+	h1 {
+		padding: 0;
+		margin: 0;
+	}
+</style>
